23a9c812a396e4cde217835742012812760d4a1f
nhloder <nhloder@gmail.com>
2019-10-01 14:22:22 -0600

    day2

--- practice.js
+++ practice.js
@@ -2,31 +2,31 @@
 
 //Create a variable called myName that is a string data type
 
-//Code here
+ 
 
 //////////////////PROBLEM 2////////////////////
 
 //Create a variable called myAge that is a number data type
 
-//Code here
+ 
 
 //////////////////PROBLEM 3////////////////////
 
 //Create a variable called lovesCode that is a boolean data type
 
-//Code here
+ 
 
 //////////////////PROBLEM 4////////////////////
 
 //Create a variable called greatestFear that is undefined because we fear nothing
 
-//Code here
+ 
 
 //////////////////PROBLEM 5////////////////////
 
 //Create a variable called devMountainGoal that is null because we are just starting out
 
-//Code here
+ 
 
 //////////////////PROBLEM 6////////////////////
 
@@ -35,21 +35,21 @@
 //greeting should return the string "Hello, "
 //plus the value of the name parameter.
 
-//Code here
+ 
 
 //////////////////PROBLEM 7////////////////////
 
 //Rewrite the function greeting as a function expression.
 //Name it newGreeting.
 
-//Code Here
+ 
 
 //////////////////PROBLEM 8////////////////////
 
 //Create an array called groceries with the values
 //"apples", "milk", "eggs", "bread"
 
-//Code Here
+ 
 
 //////////////////PROBLEM 9////////////////////
 
@@ -58,12 +58,12 @@
 //name (a string), color (a string), age (a number),
 //and goodBoy (a boolean).
 
-//Code Here
+ 
 
 //...access the dog's name from the object and assign it to a
 //variable called devMountainClassPet.
 
-//Code Here
+ 
 
 //////////////////PROBLEM 10////////////////////
 
@@ -82,12 +82,12 @@
 // that will be numbers.
 // The add function should return the two parameters added together
 
-//Code Here
+ 
 
 //Now invoke add, passing in the numbers 3 and 4
 //storing the result in the variable mathSum.
 
-//Code Here
+ 
 
 //////////////////PROBLEM 12////////////////////
 
